a64da5967bb6020918306cfe5686e9859b8a0d39
MrArcano <salvatore-dario@hotmail.it>
2023-11-06 19:40:23 +0100

    [edit]: db movie

--- Models/db.php
+++ Models/db.php
@@ -5,13 +5,25 @@ $movies = [
               ["Action","Adventure","Fantasy","Crime"], 
               1989, 
               4.5, 
-              new Media ("img/batman.jpg","Batman")),
+              new Media ("img/batman.jpg","Batman"),
+              [
+                new Actor("Michael","Keaton",70,"M"),
+                new Actor("Jack","Nicholson",84,"M"),
+              ]
+            ),
 
   new Movie(  "Spiderman", 
               ["Action","Adventure","Fantasy","Science Fiction"], 
               2002, 
               4,
-              new Media ("img/spiderman.jpg","Spiderman")),
+              new Media ("img/spiderman.jpg","Spiderman"),
+              [
+                new Actor("Tobey","Maguire", 46, "M"),
+                new Actor("Kirsten","Dunst", 39 , "F"),
+                new Actor("Willem","Dafoe", 66 , "M"),
+                new Actor("James","Franco", 43 , "M"),
+              ]
+            ),
 
   new Movie(  "Pearl Harbor", 
               ["Drama","Romance","Action","Adventure","War"], 
@@ -19,11 +31,12 @@ $movies = [
               1.5, 
               new Media ("img/pearl_harbor.jpg","Pearl Harbor"),
               [
-                new Actor("Pinco", "Pallo", 33),
-                new Actor("Panco", "Pillo", 22 , "M"),
-                new Actor("Lady", "Mary", 26 , "F"),
-              ]),
-  new Movie ("nome", ["genere"],9999,5)
+                new Actor("Ben","Affleck", 48, "M"),
+                new Actor("Josh","Hartnett", 42 , "M"),
+                new Actor("Kate","Beckinsale", 47 , "F"),
+              ]
+            ),
+  new Movie ("Title", ["genres"],1999,2)
 ];
 
 ?>
