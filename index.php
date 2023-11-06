7f34ab121b1cce308a47c0e22c20ba18d6df143c
MrArcano <salvatore-dario@hotmail.it>
2023-11-06 15:47:38 +0100

    [edit]: movies

--- index.php
+++ index.php
@@ -16,8 +16,11 @@ class Actor{
   public $gender;
   public $age;
 
-  public function __construct(){
-    
+  public function __construct(string $_name,string $_lastname,int $_age,string $_gender = "NaN"){
+    $this->name = $_name;
+    $this->lastname = $_lastname;
+    $this->age = $_age;
+    $this->gender = $_gender;
   }
 };
 
@@ -29,7 +32,7 @@ class Movie{
   public $image;
   public $actors;
 
-  public function __construct(string $_title,string $_category,int $_year,float $_rating, Media $_image = null, Actor $_actors = null){
+  public function __construct(string $_title,array $_category,int $_year,float $_rating, Media $_image = null, array $_actors = null){
     $this->title = $_title;
     $this->category = $_category;
     $this->year = $_year;
@@ -40,9 +43,31 @@ class Movie{
 };
 
 $movies = [
-  new Movie("Batman", "Action", 1993, 6.4),
-  new Movie("Spiderman", "Action", 2023, 8.1),
+  new Movie(  "Batman", 
+              ["Action","Adventure","Fantasy","Crime"], 
+              1989, 
+              4.5, 
+              new Media ("https://cps-static.rovicorp.com/2/Open/Warner_Brothers_360/Program/56121/_derived_jpg_q90_310x470_m0/56121_Batman_PA.jpg","Batman")),
+
+  new Movie(  "Spiderman", 
+              ["Action","Adventure","Fantasy","Science Fiction"], 
+              2002, 
+              4,
+              new Media ("https://cps-static.rovicorp.com/2/Open/Sony%20Pictures/Spider-Man/_derived_jpg_q90_310x470_m0/SpiderMan-PosterArt.jpg","Spiderman")),
+
+  new Movie(  "Pearl Harbor", 
+              ["Drama","Romance","Action","Adventure","War"], 
+              2001, 
+              1.5, 
+              new Media ("https://cps-static.rovicorp.com/2/Open/EU%20VOD%20Releases/Pearl%20Harbor/_derived_jpg_q90_310x470_m0/Pearl_1sheetB_XL.jpg","Pearl Harbor"),
+              [
+                new Actor("Pinco", "Pallo", 33 , "M"),
+                new Actor("Panco", "Pillo", 22 , "M"),
+                new Actor("Lady", "Mary", 26 , "F"),
+              ]),
 ];
+
+
 
 var_dump($movies);
 
