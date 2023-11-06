6915610148706a9ed427da05411a037d9a935f99
MrArcano <salvatore-dario@hotmail.it>
2023-11-06 19:21:48 +0100

    [add]: img file

--- Models/db.php
+++ Models/db.php
@@ -5,19 +5,19 @@ $movies = [
               ["Action","Adventure","Fantasy","Crime"], 
               1989, 
               4.5, 
-              new Media ("https://cps-static.rovicorp.com/2/Open/Warner_Brothers_360/Program/56121/_derived_jpg_q90_310x470_m0/56121_Batman_PA.jpg","Batman")),
+              new Media ("img/batman.jpg","Batman")),
 
   new Movie(  "Spiderman", 
               ["Action","Adventure","Fantasy","Science Fiction"], 
               2002, 
               4,
-              new Media ("https://cps-static.rovicorp.com/2/Open/Sony%20Pictures/Spider-Man/_derived_jpg_q90_310x470_m0/SpiderMan-PosterArt.jpg","Spiderman")),
+              new Media ("img/spiderman.jpg","Spiderman")),
 
   new Movie(  "Pearl Harbor", 
               ["Drama","Romance","Action","Adventure","War"], 
               2001, 
               1.5, 
-              new Media ("https://cps-static.rovicorp.com/2/Open/EU%20VOD%20Releases/Pearl%20Harbor/_derived_jpg_q90_310x470_m0/Pearl_1sheetB_XL.jpg","Pearl Harbor"),
+              new Media ("img/pearl_harbor.jpg","Pearl Harbor"),
               [
                 new Actor("Pinco", "Pallo", 33),
                 new Actor("Panco", "Pillo", 22 , "M"),
--- index.php
+++ index.php
@@ -6,15 +6,6 @@ require_once __DIR__ . "/Models/Movie.php";
 
 require_once __DIR__ . "/Models/db.php";
 
-
-// var_dump($movies[2]);
-
-// var_dump($movies[3]);
-
-$movies[3] -> setImage(new Media("Http","Nome"));
-
-// var_dump(implode(" - ",$movies[2] -> actors)-> name);
-
 ?>
 
 <!DOCTYPE html>
@@ -26,26 +17,37 @@ $movies[3] -> setImage(new Media("Http","Nome"));
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+  <!-- Custom css -->
+  <link rel="stylesheet" href="css/style.css">
   <title>PHP MOVIE OOP</title>
 </head>
 <body>
   <h1 class=" text-center ">PHP MOVIE OOP</h1>
 
-  <div class="container d-flex ">
+  <div class="container d-flex gap-3 ">
 
   <?php foreach ($movies as $movie):?>
 
     <!-- card -->
     <div class="card" style="width: 18rem;">
-      <img src="<?php echo $movie -> image -> src_image?>" class="card-img-top" alt="<?php echo $movie -> image -> name_image?>">
+      <img src="<?php echo $movie -> image -> src_image ?? 'img/no_poster.jpg'?>" class="card-img-top" alt="<?php echo $movie -> image -> name_image ?? 'NotFound'?>">
       <div class="card-body">
-        <h5 class="card-title"><?php echo $movie -> title ?></h5>
-        <p class="card-text"><?php echo $movie -> year?></p>
-        <p class="card-text"><?php echo $movie -> rating?></p>
-        <p class="card-text"><?php echo implode(" - ",$movie -> category)?></p>
+        <h4 class="card-title"><?php echo $movie -> title ?></h4>
+        <p class="card-text"><strong>Year:</strong> <?php echo $movie -> year?></p>
+        <p class="card-text"><strong>Rating:</strong> <?php echo $movie -> rating?></p>
+        <p class="card-text"><strong>Genres:</strong> <?php echo implode(" - ",$movie -> category)?></p>
 
-        <?php foreach ($movie -> actors as $actor): ?>
-          <span class="card-text"><?php echo $actor -> name ? ($actor -> name)." - " : ("undefined") ?></span>
+        <!-- se l'array degli attori è vuoto allora scrivo undefined -->
+        <p class="card-text">
+          <strong>Actors: </strong><?php echo count($movie -> actors) < 1 ? "undefined" : "" ; ?>
+        </p>
+        
+        <?php foreach ($movie -> actors as $index => $actor): ?>
+          <p class="card-text">
+            <?php 
+              echo $actor -> name . " " . $actor -> lastname . ", Age: " . $actor -> age . ", Gender: " . $actor -> gender;
+            ?>
+          </p>
         <?php endforeach ?>
         
       </div>
