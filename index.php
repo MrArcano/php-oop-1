ebce473106234d9f3e803dc98742066030b48931
MrArcano <salvatore-dario@hotmail.it>
2023-11-08 17:35:05 +0100

    [edit]: style

--- index.php
+++ index.php
@@ -40,46 +40,53 @@ try {
 
     <?php else: ?>
 
-      <div class="row row-cols-3">
+      <div class="row">
         <?php foreach ($productions as $production):?>
           <div class="col py-3">
             <!-- card -->
-            <div class="card bg-dark-subtle m-auto h-100 " style="width: 18rem;">
-              <img src="<?php echo $production -> image -> src_image ?? 'img/no_poster.jpg'?>" class="card-img-top" alt="<?php echo $production -> image -> name_image ?? 'NotFound'?>">
-              <div class="card-body">
-                <h6 class="card-text text-center mb-3 text-bg-dark"><?php echo get_class($production) ?></h6>
-                <h4 class="card-title"><?php echo $production -> title ?></h4>
-                <p class="card-text">
-                  <strong>Year: </strong>
-                  <?php echo $production -> getYearStr()?>
-                </p>
-  
-                <?php if(get_class($production) === "TvSerie"): ?>
-                  <p class="card-text">
-                    <span class="me-3"><strong>Seasons: </strong><?php echo $production -> number_of_seasons ?></span>
-                    <span><strong>Episodes: </strong><?php echo $production -> number_of_episodes ?></span>
-                  </p>
-                <?php endif ?>
-  
-        
-                <p class="card-text"><strong>Rating:</strong> <?php echo $production -> rating?></p>
-                <p class="card-text"><strong>Genres:</strong> <?php echo implode(" - ",$production -> category)?></p>
-        
-                <!-- se l'array degli attori è vuoto allora scrivo undefined -->
-                <p class="card-text">
-                  <strong>Actors: </strong><?php echo count($production -> actors) < 1 ? "undefined" : "" ; ?>
-                </p>
-  
-                <ul>
-                  <?php foreach ($production -> actors as $index => $actor): ?>
-                    <li class="card-text">
-                      <?php 
-                        echo $actor -> get_FullName();
-                      ?>
-                    </li>
-                  <?php endforeach ?>
-                </ul>
-  
+            <div class="card bg-dark-subtle m-auto" style="width: 600px;height: 300px;">
+              <div class="row g-0">
+                <div class="col-4">
+                  <img src="<?php echo $production -> image -> src_image ?? 'img/no_poster.jpg'?>" class="img-fluid rounded-start" alt="<?php echo $production -> image -> name_image ?? 'NotFound'?>">
+                </div>
+              
+                <div class="col-8">
+                  <div class="card-body">
+                    <h6 class="card-text text-center text-bg-dark mb-2"><?php echo get_class($production) ?></h6>
+                    <h4 class="card-title"><?php echo $production -> title ?></h4>
+                    <p class="card-text">
+                      <strong>Year: </strong>
+                      <span class="text-14"><?php echo $production -> getYearStr()?></span>
+                    </p>
+      
+                    <?php if(get_class($production) === "TvSerie"): ?>
+                      <p class="card-text">
+                        <strong>Seasons: </strong><span class="text-14 me-3"><?php echo $production -> number_of_seasons ?></span>
+                        <strong>Episodes: </strong><span class="text-14"><?php echo $production -> number_of_episodes ?></span>
+                      </p>
+                    <?php endif ?>
+      
+            
+                    <p class="card-text"><strong>Rating:</strong> <span class="text-14"><?php echo $production -> rating?></span></p>
+                    <p class="card-text"><strong>Genres:</strong> <span class="text-14"><?php echo implode(" - ",$production -> category)?></span></p>
+            
+                    <!-- se l'array degli attori è vuoto allora scrivo undefined -->
+                    <p class="card-text">
+                      <strong>Actors: </strong><?php echo count($production -> actors) < 1 ? "undefined" : "" ; ?>
+                    </p>
+      
+                    <ul>
+                      <?php foreach ($production -> actors as $index => $actor): ?>
+                        <li class="card-text text-14">
+                          <?php 
+                            echo $actor -> get_FullName();
+                          ?>
+                        </li>
+                      <?php endforeach ?>
+                    </ul>
+      
+                  </div>
+                </div>
               </div>
             </div>
           </div>
