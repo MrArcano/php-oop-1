cb7ef3805f6270791c0f55dccf725ac57ddd251e
MrArcano <salvatore-dario@hotmail.it>
2023-11-08 16:13:45 +0100

    [add]: throw new Exception, try & catch

--- Models/Production.php
+++ Models/Production.php
@@ -8,11 +8,15 @@ class Production {
   public $actors;
 
   public function __construct(string $_title,array $_category,float $_rating, Media $_image = null, array $_actors = []){
-    $this->title = $_title;
-    $this->category = $_category;
-    $this->rating = $_rating;
-    $this->image = $_image;
-    $this->actors = $_actors;
+    if(empty($_title) || empty($_category) || empty($_rating) || empty($_image)){
+      throw new Exception("Dati non inseriti correttamente");
+    }else{
+      $this->title = $_title;
+      $this->category = $_category;
+      $this->rating = $_rating;
+      $this->image = $_image;
+      $this->actors = $_actors;
+    }
   }
 }
 
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php 
 
-require_once __DIR__ ."/Traits/PublishedYear.php";
-
-require_once __DIR__ . "/Models/Production.php";
-require_once __DIR__ . "/Models/Movie.php";
-require_once __DIR__ . "/Models/TvSerie.php";
-require_once __DIR__ . "/Models/Media.php";
-require_once __DIR__ . "/Models/Actor.php";
-
-require_once __DIR__ . "/Models/db.php";
-
+try {
+  require_once __DIR__ ."/Traits/PublishedYear.php";
+  require_once __DIR__ . "/Models/Production.php";
+  require_once __DIR__ . "/Models/Movie.php";
+  require_once __DIR__ . "/Models/TvSerie.php";
+  require_once __DIR__ . "/Models/Media.php";
+  require_once __DIR__ . "/Models/Actor.php";
+  require_once __DIR__ . "/Models/db.php";
+} catch (Exception $e) {
+  $error_msg = $e->getMessage();
+}
 
 ?>
 
@@ -30,51 +31,63 @@ require_once __DIR__ . "/Models/db.php";
   <h1 class=" text-center ">PHP MOVIE OOP</h1>
 
   <div class="container">
-    <div class="row row-cols-3">
-      <?php foreach ($productions as $production):?>
-        <div class="col py-3">
-          <!-- card -->
-          <div class="card bg-dark-subtle m-auto h-100 " style="width: 18rem;">
-            <img src="<?php echo $production -> image -> src_image ?? 'img/no_poster.jpg'?>" class="card-img-top" alt="<?php echo $production -> image -> name_image ?? 'NotFound'?>">
-            <div class="card-body">
-              <h6 class="card-text text-center mb-3 text-bg-dark"><?php echo get_class($production) ?></h6>
-              <h4 class="card-title"><?php echo $production -> title ?></h4>
-              <p class="card-text">
-                <strong>Year: </strong>
-                <?php echo $production -> getYearStr()?>
-              </p>
 
-              <?php if(get_class($production) === "TvSerie"): ?>
+    <?php if(isset($error_msg)): ?>
+
+      <div class="alert alert-light text-center" role="alert">
+        <?php echo $error_msg ?>
+      </div>
+
+    <?php else: ?>
+
+      <div class="row row-cols-3">
+        <?php foreach ($productions as $production):?>
+          <div class="col py-3">
+            <!-- card -->
+            <div class="card bg-dark-subtle m-auto h-100 " style="width: 18rem;">
+              <img src="<?php echo $production -> image -> src_image ?? 'img/no_poster.jpg'?>" class="card-img-top" alt="<?php echo $production -> image -> name_image ?? 'NotFound'?>">
+              <div class="card-body">
+                <h6 class="card-text text-center mb-3 text-bg-dark"><?php echo get_class($production) ?></h6>
+                <h4 class="card-title"><?php echo $production -> title ?></h4>
                 <p class="card-text">
-                  <span class="me-3"><strong>Seasons: </strong><?php echo $production -> number_of_seasons ?></span>
-                  <span><strong>Episodes: </strong><?php echo $production -> number_of_episodes ?></span>
+                  <strong>Year: </strong>
+                  <?php echo $production -> getYearStr()?>
                 </p>
-              <?php endif ?>
-
-      
-              <p class="card-text"><strong>Rating:</strong> <?php echo $production -> rating?></p>
-              <p class="card-text"><strong>Genres:</strong> <?php echo implode(" - ",$production -> category)?></p>
-      
-              <!-- se l'array degli attori è vuoto allora scrivo undefined -->
-              <p class="card-text">
-                <strong>Actors: </strong><?php echo count($production -> actors) < 1 ? "undefined" : "" ; ?>
-              </p>
-
-              <ul>
-                <?php foreach ($production -> actors as $index => $actor): ?>
-                  <li class="card-text">
-                    <?php 
-                      echo $actor -> get_FullName();
-                    ?>
-                  </li>
-                <?php endforeach?>
-              </ul>
-
+  
+                <?php if(get_class($production) === "TvSerie"): ?>
+                  <p class="card-text">
+                    <span class="me-3"><strong>Seasons: </strong><?php echo $production -> number_of_seasons ?></span>
+                    <span><strong>Episodes: </strong><?php echo $production -> number_of_episodes ?></span>
+                  </p>
+                <?php endif ?>
+  
+        
+                <p class="card-text"><strong>Rating:</strong> <?php echo $production -> rating?></p>
+                <p class="card-text"><strong>Genres:</strong> <?php echo implode(" - ",$production -> category)?></p>
+        
+                <!-- se l'array degli attori è vuoto allora scrivo undefined -->
+                <p class="card-text">
+                  <strong>Actors: </strong><?php echo count($production -> actors) < 1 ? "undefined" : "" ; ?>
+                </p>
+  
+                <ul>
+                  <?php foreach ($production -> actors as $index => $actor): ?>
+                    <li class="card-text">
+                      <?php 
+                        echo $actor -> get_FullName();
+                      ?>
+                    </li>
+                  <?php endforeach ?>
+                </ul>
+  
+              </div>
             </div>
           </div>
-        </div>
-      <?php endforeach?>
-    </div>
+        <?php endforeach ?>
+      </div>
+
+    <?php endif ?>
+
   </div>
 
 </body>
